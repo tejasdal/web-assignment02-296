ac4cf5400ea1ec09f0ec6f6c94062b4c9201724e
Tejas Patel <tejas.patel@dal.ca>
2020-06-12 19:32:48 -0300

    Added changes in package.json and added server.js file to deploy application.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+ 
+const app = express();
+ 
+app.use(express.static(__dirname + '/web-assignment02'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/web-assignment02/index.html'));
+});
+ 
+app.listen(process.env.PORT || 8080);
